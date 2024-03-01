f94a8476cfb7164b6d5159a0b554efca96189af9
notnguyen <149485902+notnguyen@users.noreply.github.com>
2024-03-01 08:45:17 +0100

    Add files via upload

---
+++ 20231222/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace _20231222
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "C:\\Users\\nguyen4\\source\\repos\\20231222\\text.txt";
+
+            using (StreamWriter sw = new StreamWriter(file))
+            {
+                Console.WriteLine("Sign in");
+                string username = Console.ReadLine();
+                string password = Console.ReadLine();
+
+                sw.WriteLine(username + ":" + password);
+            }
+
+            using (StreamReader sr = new StreamReader(file))
+            {
+                Console.WriteLine("Log in");
+                string user = Console.ReadLine();
+                string pass = Console.ReadLine();
+
+
+                string line = sr.ReadLine();
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] split = line.Split(':');
+                    string First = split[0];
+                    string Second = split[1];
+
+                    Console.WriteLine(First + ":" + Second);
+
+                }
+
+                
+
+            }
+
+            
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ 20240105/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+
+namespace _20240105
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "C:\\Users\\nguyen4\\source\\repos\\20240105\\timetable.csv";
+
+            try
+            {
+                
+                using (StreamReader sr = new StreamReader(file))
+                {
+                    string line;
+                    
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
