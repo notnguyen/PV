8595485c413aba440ed4bb1bbfd0ef4957f00fc3
notnguyen <149485902+notnguyen@users.noreply.github.com>
2024-09-17 11:06:59 +0200

    Add files via upload

---
+++ Collections/set.py
@@ -0,0 +1,17 @@
+set1 = {"a", "b", "c", "a", None}
+set2 = {5, 1, 3, 5, None}
+set3 = {True, False, False, None}
+set4 = {"b", 3, True}
+
+print("set: " + str(set1))
+print("set2: " + str(set2))
+print("set3: " + str(set3))
+
+# print(set1[3])
+
+print(len(set1))
+
+set5 = {set1, "b", 3, True}
+
+print("set5: " + str(set5))
+
---
+++ Collections/string.py
@@ -0,0 +1,9 @@
+# string1 = "a", "b", "c", "a"
+# string2 = "5", "1", "3", "5"
+# string3 = "True", "False", "False"
+# string4 = "b", "3", "True"
+#
+# print("string: " + str(string1))
+# print("string2: " + str(string2))
+# print("string3: " + str(string3))
+# print("string4: " + str(string4))
