d0b7d7f8f85bf12bc676fd81038bf7c9e622dc55
notnguyen <nguyen4@spsejecna.cz>
2024-12-08 21:19:45 +0100

    commit

---
+++ Parallel programming/12.1_Start_of_parallel_process.py
@@ -0,0 +1,11 @@
+import time
+
+def vypis_cisel():
+    for i in range(0,10):
+        print(i)
+        time.sleep(1)
+
+if __name__ == "__main__":
+    print("ZACATEK PROGRAMU")
+    vypis_cisel()
+    print("KONEC PROGRAMU")
